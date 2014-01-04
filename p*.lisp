db440c1100679ac7c0a02917463209695a38ae3e
ThisIsGJ <guan_jun@live.cn>
2014-01-04 13:42:36 +0000

    final_2

--- p*.lisp
+++ p*.lisp
@@ -1,12 +1,80 @@
+(defun poly+ (poly1 poly2)
+
+	(format t "~%poly1 ~A" poly1)
+	(format t "~%poly2 ~A" poly2)
+
+	(let ((l1 poly1) (l2 poly2))
+		(labels (
+			;'((1 2 x)) '((1 2 y))
+			 	(main (p1 p2) 
+			 		(if (null p1)
+			 			()
+			 			(let ((pass (test (car p1) p2))) 
+			 				 (if (listp pass) 
+			 				 	(cons pass (main (cdr p1) p2))
+			 				 	(cons (car p1) (main (cdr p1) p2)))
+			 				)))
+			 	;'(1 2 x) '((1 2 y))
+				(test (l1 l2) (if (null l2)
+	 					t
+	 					(if (test2 (cdr l1) (cdar l2))	;size!!!一样测，不一样不测
+	 						(cons (+ (car l1) (caar l2)) (cdr l1))
+	 						(test l1 (cdr l2)))
+	 						)
+
+	 					)
+				;'(2 x 2 y) '(2 y 2 x)
+				(test2 (l1 l2) (if (null l1)
+								t				
+								(if (test3 (cons (car l1) (list (cadr l1))) l2)	
+									(if (= 2 (list-length l1))
+										(test2 () l2)
+										(test2 (cddr l1) l2))
+									nil	
+									)									
+								
+					 )
+				)
+				;'(2 x) '(2 y 2 x)
+				(test3 (l1 l2) (if (null l2)
+								nil
+								(if (equal l1 (cons (car l2) (list (cadr l2))))
+									t
+									(if (= 2 (list-length l2))
+										(test3 l1 ())
+										(test3 l1 (cddr l2))
+										
+									
+									)
+								)
+					))
+				(result (l1 l2) (if (null l1)
+									l2
+									(if(listp (test (car l1) l2))
+										(result (cdr l1) l2)
+										(cons (car l1) (result (cdr l1) l2))
+										
+										)
+									)									
+					)	
+		)
+		(result l2 (main l1 l2))
+
+		)
+		)
+)
+
+
+
 (defun poly*(poly1 poly2)
 
 	(format t "~%poly1 ~A" poly1)
 	(format t "~%poly2 ~A" poly2)
-	
+
 	(let ((l1 poly1) (l2 poly2))
 		(labels ((addf (l1 l2) (if (null l2)
 									()
-									(cons (cons (+ (car l1) (caar l2)) (cdar l2)) (addf l1 (cdr l2)))))
+									(cons (cons (* (car l1) (caar l2)) (cdar l2)) (addf l1 (cdr l2)))))
 
 				(addr (l1 l2) (if (null l1)
 									l2
@@ -29,12 +97,24 @@
 								(if (equal e1 (car l2))
 											(cons e1 (test2 e1 v1 (cons (+ v1 (cadr l2)) (cddr l2))))
 											;(cons (cons e1 (+ v1 (cadr l2))) (test2 e1 v1 (cddr l2)))
-											(cons (car l2) (test2 e1 v1 (cdr l2)))
-											))))
-			(rev(addr (reverse l1) (rev (addf l1 l2)))))))
+											;(cons e1 (cons v1 (test2 e1 v1 (cdr l2))))
+											(if (numberp (car l2))
+												(cons (car l2) (test2 e1 v1 (cdr l2)))
+												(cons e1 (cons v1 (cons (car l2) (test2 e1 v1 (cdr l2)))))
+												)
+											)))
+				(addp (l1 l2) (if (null l1)
+								()
+								(poly+ (rev(addr (reverse (car l1)) (rev (addf (car l1) l2)))) (addp (cdr l1) l2))
+
+
+								)
+				) ) (addp l1 l2)
+					) ))
+
 
 (defun test()
-	(poly* '(2 2 x 2 y) '((2 3 x 1 y) (3 3 y)))
+	(poly* '((1 1 y) (1 1 x)) '((1 1 x) (1 1 y)))
 	)
 
 
--- p+_3.lisp
+++ p+_3.lisp
@@ -6,6 +6,7 @@
 
 	(let ((l1 poly1) (l2 poly2))
 		(labels (
+			;'((1 2 x)) '((1 2 y))
 			 	(main (p1 p2) 
 			 		(if (null p1)
 			 			()
@@ -14,13 +15,40 @@
 			 				 	(cons pass (main (cdr p1) p2))
 			 				 	(cons (car p1) (main (cdr p1) p2)))
 			 				)))
-
+			 	;'(1 2 x) '((1 2 y))
 				(test (l1 l2) (if (null l2)
 	 					t
-	 					(if (equal (cdr l1) (cdar l2)) 
+	 					(if (test2 (cdr l1) (cdar l2))	;size!!!一样测，不一样不测
 	 						(cons (+ (car l1) (caar l2)) (cdr l1))
 	 						(test l1 (cdr l2)))
-	 					))
+	 						)
+
+	 					)
+				;'(2 x 2 y) '(2 y 2 x)
+				(test2 (l1 l2) (if (null l1)
+								t				
+								(if (test3 (cons (car l1) (list (cadr l1))) l2)	
+									(if (= 2 (list-length l1))
+										(test2 () l2)
+										(test2 (cddr l1) l2))
+									nil	
+									)									
+								
+					 )
+				)
+				;'(2 x) '(2 y 2 x)
+				(test3 (l1 l2) (if (null l2)
+								nil
+								(if (equal l1 (cons (car l2) (list (cadr l2))))
+									t
+									(if (= 2 (list-length l2))
+										(test3 l1 ())
+										(test3 l1 (cddr l2))
+										
+									
+									)
+								)
+					))
 				(result (l1 l2) (if (null l1)
 									l2
 									(if(listp (test (car l1) l2))
@@ -38,7 +66,7 @@
 )
 
 (defun test()
-	(poly+ '((-1 1 x)) '((2 1 z)))
+	(poly+ '((1 2 x 2 y)) '((1 2 y 2 x)))
 	)
 
 
